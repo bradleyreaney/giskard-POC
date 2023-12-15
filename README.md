075a09259e9b5e9511a66e2f143ff16573dd6861
Brad <bradley.reaney@nimbleapproach.com>
2023-12-15 11:03:43 +0000

    Working state

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # giskard-POC
 Proof of concept for Giskard
+
+# Setup
+
+1. Set up python virtual directory with `python -m venv .venv`
+2. Activate the virtual env from the terminal by run `source .venv/bin/activate`
+3. Install requirements using the command `pip install -r requirements.txt`
+
+# Note
+- If you have an error about `attr.s`, uninstalling and reinstalling `attrs` fixed it.
---
+++ src/app.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+import pandas as pd
+from scipy.special import softmax
+from datasets import load_dataset
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from datetime import datetime
+
+from giskard import (Dataset, Model, scan, testing, GiskardClient, Suite)
+
+MODEL_NAME = "cardiffnlp/twitter-roberta-base-sentiment"
+
+# All based on hugging fast datasets
+# Path = Dataset's name
+# Name = Subset
+DATASET_CONFIG = {"path": "tweet_eval", "name": "sentiment", "split": "validation"}
+# DATASET_CONFIG = {"path": "consumer_health_questions", "split": "validation"}
+
+LABEL_MAPPING = {0: "negative", 1: "neutral", 2: "positive"}
+
+TEXT_COLUMN = "text"
+TARGET_COLUMN = "label"
+
+raw_data = load_dataset(**DATASET_CONFIG).to_pandas().iloc[:500]
+raw_data = raw_data.replace({"label": LABEL_MAPPING})
+
+giskard_dataset = Dataset(
+    df=raw_data,
+    # A pandas.DataFrame that contains the raw data (before all the pre-processing steps) and the actual ground truth variable (target).
+    target=TARGET_COLUMN,  # Ground truth variable.
+    # name="Tweets with sentiment",  # Optional.
+    name="Health related questions"
+)
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+
+
+def prediction_function(df: pd.DataFrame) -> np.ndarray:
+    encoded_input = tokenizer(list(df[TEXT_COLUMN]), padding=True, return_tensors="pt")
+    output = model(**encoded_input)
+    return softmax(output["logits"].detach().numpy(), axis=1)
+
+
+giskard_model = Model(
+    model=prediction_function,  # A prediction function that encapsulates all the data pre-processing steps and that
+    model_type="classification",  # Either regression, classification or text_generation.
+    name="RoBERTa for sentiment classification",  # Optional
+    classification_labels=list(LABEL_MAPPING.values()),  # Their order MUST be identical to the prediction_function's
+    feature_names=[TEXT_COLUMN],  # Default: all columns of your dataset
+)
+
+results = scan(giskard_model, giskard_dataset)
+
+# Generate the output path if it doesn't exist
+isExist = os.path.exists("../output")
+
+if not isExist:
+    os.makedirs("../output")
+
+now = datetime.now()
+
+dt_string = now.strftime("%d-%m-%Y-%H:%M:%S")
+
+# Save it to a file
+# results.to_html("../output/scan_report.html")
+results.to_html(f'../output/scan_report_{dt_string}.html')
